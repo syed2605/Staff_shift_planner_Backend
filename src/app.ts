af4f4b409f86586bb96eee4c6c3d367e1a5ad468
gousesyed97@gmail.com <gousesyed97@gmail.com>
2025-05-19 12:49:23 +0530

    status codes messages & base model

--- src/app.ts
+++ src/app.ts
@@ -9,5 +9,16 @@ const app = express();
 app.use(cors);
 app.use(express.json());
 app.use(cookieParser());
+app.use((req: Request, res: Response, next: NextFunction) => {
+    logger.info(`Incoming ${req.method} request to ${req.url}`);
+    next();
+  });
+app.use(limiter);
+app.use(helmet());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`Server running on http://localhost:3000`);
+});
+
 
 export default app;
