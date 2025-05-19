38b4d08cb36049a75d33bb60cb5f72db025de9a9
gousesyed97@gmail.com <gousesyed97@gmail.com>
2025-05-19 13:46:34 +0530

    validation done route

--- src/route/staff.routes.ts
+++ src/route/staff.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import * as staffController from '../controller/staff.controller';
-import { validateBody } from '../middleware/validate';
+import { validate } from '../middleware/validate.middleware';
 import { staffCreateSchema } from '../validators/staff.validator';
 
 const router = express.Router();
 
-router.post('/', validateBody(staffCreateSchema), staffController.createStaff);
+router.post('/', validate(staffCreateSchema), staffController.createStaff);
 router.get('/', staffController.getAllStaff);
 
 export default router;
